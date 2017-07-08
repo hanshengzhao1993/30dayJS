39638cf8d18bbdca0c7c3d29a571ce94c7b074d9
Han <hanshengzhao1993@gmail.com>
2017-07-07 23:13:33 -0700

    implemented finding the div with key press

--- day1/main.js
+++ day1/main.js
@@ -1,5 +1,8 @@
 window.onload = function () {
   window.addEventListener('keydown', function (e) {
     console.log(e.keyCode);
+    var keyCode = e.keyCode;
+    var selectDiv = document.querySelector(`div [key-code="${keyCode}"]`);
+    console.log(selectDiv);
   })
 }
