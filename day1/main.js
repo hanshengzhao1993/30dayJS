5b2bbed1d6683ba7f4d2b79983d828d17e2c823b
Han <hanshengzhao1993@gmail.com>
2017-07-08 00:01:57 -0700

    Finished first project

--- day1/main.js
+++ day1/main.js
@@ -1,8 +1,23 @@
 window.onload = function () {
   window.addEventListener('keydown', function (e) {
-    console.log(e.keyCode);
     var keyCode = e.keyCode;
     var selectDiv = document.querySelector(`div [key-code="${keyCode}"]`);
+    if(!selectDiv){
+      return;
+    }
+    var audio = selectDiv.querySelector('audio');
+    audio.currentTime = 0;
+    audio.play();
+
+    selectDiv.classList.add('playing');
+
+    setTimeout(function () {
+      selectDiv.classList.remove('playing');
+
+    },300)
+
+    // selectDiv.audio
     console.log(selectDiv);
+    console.log(audio);
   })
 }
