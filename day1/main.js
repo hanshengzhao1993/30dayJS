9bd097557d48dd16da445222e52c2dc24338ddf9
Han <hanshengzhao1993@gmail.com>
2017-07-07 22:57:15 -0700

    added Day 1 file

---
+++ day1/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  window.addEventListener('keydown', function (e) {
+    console.log(e);
+  })
+  // window.addEventListener('click', function (e) {
+  //   console.log(e);
+  // })
+}
